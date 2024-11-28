f118cb9ae3ea548a9dce8592c327ceddf4dfc670
AmeySwapra <ameyswapratechnologies@gmail.com>
2024-11-28 12:45:10 +0530

    secure

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -106,7 +106,7 @@ export const login = async (req, res) => {
     
     res.cookie("jwt", token, {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production",
+      secure : true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
