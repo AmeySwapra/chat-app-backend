389c5b60aa9298c69b8c8835e6e9b6b515583fe8
AmeySwapra <ameyswapratechnologies@gmail.com>
2024-12-05 10:04:30 +0530

    checking the backend

--- server.js
+++ server.js
@@ -22,13 +22,7 @@ app.use(cors({
   }));
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://chat-commune.netlify.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 
 
 app.use('/auth', authRoutes)
